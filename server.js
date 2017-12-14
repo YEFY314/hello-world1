1f747bcfa9486f4ceb7ba5b6709777fc2bee10b8
YEFY314 <yefengyu314@163.com>
2017-12-14 21:56:56 +0800

    fix bug 12-14

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.use(express.static('./public'));
 
 // Add middleware for http proxying
 var connProxy = proxy('/myapp/conn', { target: 'http://121.248.55.88:80',changeOrigin: true });//将服务器代理到localhost:8080端口上[本地服务器为localhost:3000]
-app.use('/myapp/conn/*', connProxy);//api子目录下的都是用代理
+app.use('/myapp/conn/*', connProxy);
 
 var shopsProxy = proxy('/myapp/shops', { target: 'http://121.248.55.88:80',changeOrigin: true });
 app.use('/myapp/shops/*',shopsProxy);
